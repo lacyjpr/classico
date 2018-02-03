cdb007a3b6b3d9a4012629eb5d72bba50a92ba6b
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-03 10:30:19 -0800

    set up eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
 
+import { BrowserRouter, Link } from 'react-router-dom';
 import Routes from './Routes';
 
 import reactLogo from './assets/React-icon.png';
