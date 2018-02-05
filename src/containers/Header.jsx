9c570dbc53a0c50937b30bc8106190f6c2cc7082
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-05 08:55:07 -0800

    Add hamburger/dropdown

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
       <nav className="nav">
-        <button className="nav__burger">☰</button>
+        <label htmlFor="hamburger" className="nav__hamburger">
+          ☰
+        </label>
+        <input type="checkbox" id="hamburger" role="button" />
+        <div className="nav__menu">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
