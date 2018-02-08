c9ac43b002237ad7ead4527de225f3b1e4a438ce
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-08 11:50:57 -0800

    Remove unused markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
 
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 
 import { Header } from './containers';
-import reactLogo from './assets/React-icon.png';
 
 const App = () => (
   <BrowserRouter>
     <main className="container">
       <Header />
-      <div>
-        <h1>hello world!</h1>
-        <img className="container__image" alt="react logo" src={reactLogo} />
-        <p>If you see this everything is working!</p>
-      </div>
-      <ul className="left">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
       <Routes />
     </main>
   </BrowserRouter>
