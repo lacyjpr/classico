cf6a5b05e8c251555729a0f9fe05697c2abb6ea9
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-08 12:12:10 -0800

    Add home image

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import homeImg from '../assets/home.jpg';
+
 const Home = () => (
   <div className="home">
+    <img className="home__image" alt="sepia city skyline" src={homeImg} />
     <h2>Home Page</h2>
   </div>
 );
