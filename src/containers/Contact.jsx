e57d2d347daef387c6a7bfcc60dd8cf2758e5b1c
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-08 16:58:49 -0800

    Add image to Contact component

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import contactImg from '../assets/contact.jpg';
+
 const Contact = () => (
-  <div>
+  <div className="contact">
+    <img
+      className="contact__image"
+      alt="city at night reflected in water"
+      src={contactImg}
+    />
     <h2>Contact Page</h2>
   </div>
 );
