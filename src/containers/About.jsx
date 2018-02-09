7aea1165704d9d6e87eff0bccbaa8a04ae3375f3
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-08 16:37:08 -0800

    Add image to about page

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import aboutImg from '../assets/about.jpg';
+
 const About = () => (
-  <div>
+  <div className="about">
+    <img className="about__image" alt="far away city skyline" src={aboutImg} />
     <h2>About Page</h2>
   </div>
 );
