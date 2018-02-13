0e470926ad7de8b2876f57de21a2df5069ff61c1
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-13 09:22:00 -0800

    Add Footer skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 
 import { Header } from './containers';
+import { Footer } from './containers';
 
 const App = () => (
   <BrowserRouter>
     <main className="container">
       <Header />
       <Routes />
+      <Footer />
     </main>
   </BrowserRouter>
 );
--- src/containers/index.js
+++ src/containers/index.js
@@ -8,3 +8,4 @@ export { default as About } from './About';
 export { default as Blog } from './Blog';
 export { default as Contact } from './Contact';
 export { default as Header } from './Header';
+export { default as Footer } from './Footer';
