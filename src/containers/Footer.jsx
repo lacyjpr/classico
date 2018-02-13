177866475a51e7bd188746c9f51e9efd671c1903
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-13 14:23:37 -0800

    Start footer markup & styling

---
+++ src/containers/Footer.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer">
+    <div className="footer__content">
+      <h3 className="footer__content--h3">Habitant porta aliquet</h3>
+      <div className="footer__content--gray">
+        <p>
+          Tincidunt interdum duis cras quis inceptos suscipit sed imperdiet,
+          libero molestie convallis habitant.
+        </p>
+        <p>
+          Curae mollis nullam dapibus curabitur hendrerit arcu duis, platea
+          vitae euismod diam cursus.
+        </p>
+        <p>
+          Cras aptent habitasse viverra habitant torquent cum neque bibendum
+          varius nisl nisi vitae.
+        </p>
+      </div>
+    </div>
+
+    <div className="footer__content">
+      <h3 className="footer__content--h3">Lectus ridiculus ult</h3>
+      <div className="footer__content--box">
+        <div>
+          <a href="#" className="footer__content--link">
+            Odio eros eu
+          </a>
+        </div>
+        <div>
+          <a href="#" className="footer__content--link">
+            Torquent facilisis
+          </a>
+        </div>
+        <div>
+          <a href="#" className="footer__content--link">
+            Per arcu nascetur
+          </a>
+        </div>
+        <div>
+          <a href="#" className="footer__content--link">
+            Imperdiet ullamcorper
+          </a>
+        </div>
+        <div>
+          <a href="#" className="footer__content--link">
+            Quisque tempor
+          </a>
+        </div>
+        <div>
+          <a href="#" className="footer__content--link">
+            A vel vitae
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div className="footer__content">
+      <h3 className="footer__content--h3">Vivamus varius ac</h3>
+      <div className="footer__content--box">
+        <p>
+          A vel vitae velit himenaeos sociis diam eros mollis, ornare augue
+          morbi id suscipit.
+        </p>
+        <hr />
+        <p>
+          A vel vitae velit himenaeos sociis diam eros mollis, ornare augue
+          morbi id suscipit.
+        </p>
+        <hr />
+        <p>
+          A vel vitae velit himenaeos sociis diam eros mollis, ornare augue
+          morbi id suscipit.
+        </p>
+
+        <a href="#" className="btn-like-gray">
+          Read More
+        </a>
+      </div>
+    </div>
+
+    <div className="footer__content">
+      <h3 className="footer__content--h3">Eget primis nisl</h3>
+      <div className="footer__content--box">
+        <p>
+          Felis sapien bibendum mollis magnis duis tellus pharetra, facilisis
+          nascetur hac phasellus condimentum.
+        </p>
+        <a href="#" className="btn-like-gray">
+          Browse
+        </a>
+      </div>
+    </div>
+  </div>
+);
+
+export default Footer;
