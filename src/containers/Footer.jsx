abc9e3195523d266756a031c4e71973922397a88
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-02-14 09:29:51 -0800

    Lay out footer images in a grid

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import imgOne from '../assets/100x100-1.jpg';
+import imgTwo from '../assets/100x100-2.jpg';
+import imgThree from '../assets/100x100-3.jpg';
+import imgFour from '../assets/100x100-4.jpg';
+import imgFive from '../assets/100x100-5.jpg';
+import imgSix from '../assets/100x100-6.jpg';
+
 const Footer = () => (
   <div className="footer">
     <div className="footer__content">
@@ -83,6 +90,19 @@ const Footer = () => (
     <div className="footer__content">
       <h3 className="footer__content--h3">Eget primis nisl</h3>
       <div className="footer__content--box">
+        <div className="footer__content--images">
+          <div className="footer__content--img-col">
+            <img alt="image" src={imgOne} />
+            <img alt="image" src={imgTwo} />
+            <img alt="image" src={imgThree} />
+          </div>
+          <div className="footer__content--img-col">
+            <img alt="image" src={imgFour} />
+            <img alt="image" src={imgFive} />
+            <img alt="image" src={imgSix} />
+          </div>
+        </div>
+
         <p>
           Felis sapien bibendum mollis magnis duis tellus pharetra, facilisis
           nascetur hac phasellus condimentum.
