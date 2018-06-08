4754cb9fa439b4497656408aa96f3d1fd4dbfba0
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-06-08 10:55:44 -0700

    Restructure Header directory

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,5 +2,5 @@ export { default as Home } from './Home/';
 export { default as About } from './About/';
 export { default as Blog } from './Blog/';
 export { default as Contact } from './Contact/';
-export { default as Header } from './Header';
+export { default as Header } from './Header/';
 export { default as Footer } from './Footer';
