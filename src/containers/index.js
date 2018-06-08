a03c39c10e170939f62044f0d09fc030fd0215f4
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-06-08 10:14:39 -0700

    Restructure Home directory

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,9 @@
-export { default as Home } from './Home';
-export { default as HomeInfoBox } from './HomeInfoBox';
-export { default as HomeIntroBox } from './HomeIntroBox';
-export { default as HomeRelated } from './HomeRelated';
-export { default as HomeCategories } from './HomeCategories';
-export { default as HomeUseful } from './HomeUseful';
+export { default as Home } from './Home/';
+// export { default as HomeInfoBox } from './HomeInfoBox';
+// export { default as HomeIntroBox } from './HomeIntroBox';
+// export { default as HomeRelated } from './HomeRelated';
+// export { default as HomeCategories } from './HomeCategories';
+// export { default as HomeUseful } from './HomeUseful';
 export { default as About } from './About';
 export { default as Blog } from './Blog';
 export { default as Contact } from './Contact';
