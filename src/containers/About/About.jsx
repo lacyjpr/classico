97efb3f065ea42481bda596b2884e5b33b714269
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-06-08 10:19:31 -0700

    Restructure About directory

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import aboutImg from '../assets/about.jpg';
+import aboutImg from '../../assets/about.jpg';
 
 const About = () => (
   <div className="about">
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,10 +1,5 @@
 export { default as Home } from './Home/';
-// export { default as HomeInfoBox } from './HomeInfoBox';
-// export { default as HomeIntroBox } from './HomeIntroBox';
-// export { default as HomeRelated } from './HomeRelated';
-// export { default as HomeCategories } from './HomeCategories';
-// export { default as HomeUseful } from './HomeUseful';
-export { default as About } from './About';
+export { default as About } from './About/';
 export { default as Blog } from './Blog';
 export { default as Contact } from './Contact';
 export { default as Header } from './Header';
